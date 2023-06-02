69b959a2150587eca190e3d8e374001d8aeb7168
shin4sjh <shinsharejh@gmail.com>
2023-06-02 17:32:39 +0900

    ++
    +=

--- 230230526_java/src/kh/lclass/oop/run/TestArray.java
+++ 230230526_java/src/kh/lclass/oop/run/TestArray.java
@@ -26,6 +26,7 @@ public class TestArray {
 //			personArray3[2].setAge[25]
 //			personArray3[3].setAge[25]
 
+	 
 	}
 
 }
--- 230230526_java/src/kh/lclass/oop/sample/Kh.java
+++ 230230526_java/src/kh/lclass/oop/sample/Kh.java
@@ -1,13 +1,58 @@
 package kh.lclass.oop.sample;
 
-public class Kh implements TestInterface{
-	public void khSpecail() {
-		
-	}
+public class Kh implements TestInterface {
+
+//	private static final long
+
+//	static final long serrialVersionUID
+
+	public void khSpecual() {
+		String str = "1111122222222";
+//		int c =  Interger.parseIn(str);
+		Car[] carArr = new Car[3];
+		try {
+			System.out.println("1");
+			carArr[4].setPrice(5000);
+			System.out.println("2");
+			carArr[0].setPrice(5000);
+			int a = Integer.parseInt(str); 
+			int c = 10 / 0; 
+			c = 0 / 10;
+		}	catch(Exception e) {
+			System.out.println(Integer.MAX_VALUE + "까지의 정수만 입력해 주세요");
+			e.printStackTrace();
+			}
+		}
+
+//			Car[] carArr = new Car[3];
+//			carArr[0] = new Car();
+//			carArr[0].setPrice(5000); // NullPointException
+//
+//			int a = Integer.parseInt(str); // NumberFormatException
+//
+//			int c = 10 / 0; // ArtihmeticException
+//			c = 0 / 10;
+//
+//		} catch (Exception e) {
+//			System.out.println(Integer.MAX_VALUE + "까지의 정수만 입력해 주세요");
+//			e.printStackTrace();
+//		} catch (ArithmeticException e) {
+//			System.out.println("0으로 나눈 수는 나타낼 수 없습니다.");
+//			e.printStackTrace();
+//		} catch (NullPointerException e) {
+//			e.printStackTrace();
+//		} 
+//			carArr[4].setPrice(5000);
+//		catch (ArrayIndexOutOfBoundsException e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
+
 	@Override
 	public void method1() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -21,5 +66,5 @@ public class Kh implements TestInterface{
 		// TODO Auto-generated method stub
 		return 0;
 	}
-	
+
 }
